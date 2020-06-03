3d5ca4c3d79cd6b9c18313cca6519fea83eb2b23
Tomás Ruiz López <truizlop@gmail.com>
2020-06-03 17:27:26 +0200

    Add instances of type classes for String

---
+++ Sources/BowLite/Instances/StringInstances.swift
@@ -0,0 +1,13 @@
+// MARK: Instance of Semigroup for String, using concatenation as combination method
+extension String: Semigroup {
+    public func combine(_ other: String) -> String {
+        self + other
+    }
+}
+
+// MARK: Instance of Monoid for String, using empty string as empty element
+extension String: Monoid {
+    public static var empty: String {
+        ""
+    }
+}
