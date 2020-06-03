acddd5c59433abda93350239b301935a1b3f55ac
Tomás Ruiz López <truizlop@gmail.com>
2020-06-03 17:13:14 +0200

    Extend Id with Functor methods

---
+++ Sources/BowLite/Data/Id/Id+Functor.swift
@@ -0,0 +1,29 @@
+public extension Id {
+    static func lift<B>(_ f: @escaping (Wrapped) -> B) -> (Id<Wrapped>) -> Id<B> {
+        { either in either.map(f) }
+    }
+    
+    func map<B>(_ f: @escaping (Wrapped) -> B) -> Id<B> {
+        Id<B>(f(self.value))
+    }
+    
+    func `as`<B>(_ b: B) -> Id<B> {
+        self.map(constant(b))
+    }
+    
+    func void() -> Id<Void> {
+        self.as(())
+    }
+    
+    func product<B>(_ f: @escaping (Wrapped) -> B) -> Id<(Wrapped, B)> {
+        self.map { a in (a, f(a)) }
+    }
+    
+    func tupleLeft<B>(_ b: B) -> Id<(B, Wrapped)> {
+        self.map { a in (b, a) }
+    }
+    
+    func tupleWrapped<B>(_ b: B) -> Id<(Wrapped, B)> {
+        self.map { a in (a, b) }
+    }
+}
