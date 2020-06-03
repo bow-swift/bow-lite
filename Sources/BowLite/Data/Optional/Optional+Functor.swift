4d88c06b319ac3c0308ccd75a9c855de2ec4a3a2
Tomás Ruiz López <truizlop@gmail.com>
2020-06-03 12:55:28 +0200

    Extend optional with Functor methods

---
+++ Sources/BowLite/Data/Optional/Optional+Functor.swift
@@ -0,0 +1,25 @@
+public extension Optional {
+    static func lift<B>(_ f: @escaping (Wrapped) -> B) -> (Wrapped?) -> B? {
+        { optional in optional.map(f) }
+    }
+    
+    func `as`<B>(_ b: B) -> B? {
+        self.map { _ in b }
+    }
+    
+    func void() -> Void? {
+        self.as(())
+    }
+    
+    func product<B>(_ f: @escaping (Wrapped) -> B) -> (Wrapped, B)? {
+        self.map { a in (a, f(a)) }
+    }
+    
+    func tupleLeft<B>(_ b: B) -> (B, Wrapped)? {
+        self.map { a in (b, a) }
+    }
+    
+    func tupleRight<B>(_ b: B) -> (Wrapped, B)? {
+        self.map { a in (a, b) }
+    }
+}
