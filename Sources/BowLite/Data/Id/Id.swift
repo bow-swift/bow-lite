19cd957157e0f262dda9717bef870674fec9f906
Tomás Ruiz López <truizlop@gmail.com>
2020-06-03 17:19:27 +0200

    Add instance of Semigroup for Id

--- Sources/BowLite/Data/Id/Id.swift
+++ Sources/BowLite/Data/Id/Id.swift
@@ -5,3 +5,9 @@ public struct Id<Wrapped> {
         self.value = value
     }
 }
+
+extension Id: Semigroup where Wrapped: Semigroup {
+    public func combine(_ other: Id<Wrapped>) -> Id<Wrapped> {
+        Id(self.value.combine(other.value))
+    }
+}
