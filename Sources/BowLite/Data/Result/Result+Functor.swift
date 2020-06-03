8f3e8753a7945d74feeab7fa792a0e9aff966c33
Tomás Ruiz López <truizlop@gmail.com>
2020-06-03 16:11:15 +0200

    Extend Result with Functor methods

---
+++ Sources/BowLite/Data/Result/Result+Functor.swift
@@ -0,0 +1,25 @@
+public extension Result {
+    static func lift<B>(_ f: @escaping (Success) -> B) -> (Result<Success, Failure>) -> Result<B, Failure> {
+        { either in either.map(f) }
+    }
+    
+    func `as`<B>(_ b: B) -> Result<B, Failure> {
+        self.map(constant(b))
+    }
+    
+    func void() -> Result<Void, Failure> {
+        self.as(())
+    }
+    
+    func product<B>(_ f: @escaping (Success) -> B) -> Result<(Success, B), Failure> {
+        self.map { a in (a, f(a)) }
+    }
+    
+    func tupleLeft<B>(_ b: B) -> Result<(B, Success), Failure> {
+        self.map { a in (b, a) }
+    }
+    
+    func tupleSuccess<B>(_ b: B) -> Result<(Success, B), Failure> {
+        self.map { a in (a, b) }
+    }
+}
