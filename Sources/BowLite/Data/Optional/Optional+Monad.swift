f5dec85101b7ce3d6abc8e813c0802a36e365ef4
Tomás Ruiz López <truizlop@gmail.com>
2020-06-03 13:32:58 +0200

    Extend optional with Monad methods

---
+++ Sources/BowLite/Data/Optional/Optional+Monad.swift
@@ -0,0 +1,32 @@
+public extension Optional {
+    func flatten<A>() -> A? where Wrapped == A? {
+        self.flatMap { $0 }
+    }
+    
+    func followedBy<A>(_ fa: A?) -> A? {
+        self.flatMap { _ in fa }
+    }
+    
+    func forEffect<A>(_ fa: A?) -> Wrapped? {
+        self.flatMap { wrapped in fa.as(wrapped) }
+    }
+    
+    func mproduct<A>(_ f: @escaping (Wrapped) -> A?) -> (Wrapped, A)? {
+        self.flatMap { wrapped in
+            f(wrapped).tupleLeft(wrapped)
+        }
+    }
+    
+    func `if`<A>(
+        then f: @escaping () -> A?,
+        else g: @escaping () -> A?
+    ) -> A? where Wrapped == Bool {
+        self.flatMap { boolean in
+            boolean ? f() : g()
+        }
+    }
+    
+    func flatTap<A>(_ f: @escaping (Wrapped) -> A?) -> Wrapped? {
+        self.flatMap { wrapped in f(wrapped).as(wrapped) }
+    }
+}
