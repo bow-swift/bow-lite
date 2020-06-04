3e862eb00b046f9f3ceaa34a37b5ba24fbc7d28d
Tomás Ruiz López <truizlop@gmail.com>
2020-06-04 10:48:19 +0200

    Add Try data type

---
+++ Sources/BowLite/Data/Try/Try.swift
@@ -0,0 +1,41 @@
+public enum Try<Success> {
+    case failure(Error)
+    case success(Success)
+    
+    public static func catching(_ f: @escaping () throws -> Success) -> Try<Success> {
+        do {
+            let success = try f()
+            return .success(success)
+        } catch {
+            return .failure(error)
+        }
+    }
+    
+    public func fold<A>(
+        _ ifFailure: @escaping (Error) -> A,
+        _ ifSuccess: @escaping (Success) -> A
+    ) -> A {
+        switch self {
+        case .failure(let error): return ifFailure(error)
+        case .success(let success): return ifSuccess(success)
+        }
+    }
+    
+    public var isFailure: Bool {
+        fold(constant(true), constant(false))
+    }
+    
+    public var isSuccess: Bool {
+        !isFailure
+    }
+    
+    public var failureValue: Error? {
+        fold(id, constant(nil))
+    }
+    
+    public var successValue: Success? {
+        fold(constant(nil), id)
+    }
+    
+    
+}
