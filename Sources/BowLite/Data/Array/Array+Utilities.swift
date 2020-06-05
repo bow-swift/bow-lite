59ac595976a919abf8d99b55ef41c227a3c3eeaa
Tomás Ruiz López <truizlop@gmail.com>
2020-06-05 09:27:30 +0200

    Add safe access to an Array

---
+++ Sources/BowLite/Data/Array/Array+Utilities.swift
@@ -0,0 +1,8 @@
+public extension Array {
+    subscript(safe index: Int) -> Element? {
+        guard index >= 0, index < count else {
+            return nil
+        }
+        return self[index]
+    }
+}
