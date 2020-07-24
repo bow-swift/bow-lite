5458a26a6756b8c3fe71d15cb9bf3e6a9502bdc3
Tomás Ruiz López <truizlop@gmail.com>
2020-07-24 12:21:03 +0200

    Add docs to Monoid

--- Sources/BowLite/Typeclasses/Monoid.swift
+++ Sources/BowLite/Typeclasses/Monoid.swift
@@ -1,12 +1,33 @@
+/// A monoid is an algebraic structure that has the same properties as a semigroup, with an empty element.
 public protocol Monoid: Semigroup {
+    /// Empty element.
+    ///
+    /// The empty element must obey the monoid laws:
+    ///
+    ///     a.combine(.empty) == .empty.combine(a) == a
+    ///
+    /// That is, combining any element with `empty` must return the original element.
+    ///
+    /// - Returns: A value of the implementing type satisfying the monoid laws.
     static var empty: Self { get }
 }
 
+// MARK: Monoid syntax
 public extension Monoid {
+    /// Combines a variable number of values of the implementing type, in the order provided in the call.
+    ///
+    /// - Parameters:
+    ///     - elements: Values of the implementing type.
+    /// - Returns: A single value of the implementing type representing the combination of all the parameter values.
     static func combineAll(_ elements: Self...) -> Self {
         combineAll(elements)
     }
     
+    /// Combines an array of values of the implementing type, in the order provided in the call.
+    ///
+    /// - Parameters:
+    ///     - elements: Values of the implementing type.
+    /// - Returns: A single value of the implementing type representing the combination of all the parameter values.
     static func combineAll(_ elements: [Self]) -> Self {
         elements.reduce(Self.empty, Self.combine)
     }
