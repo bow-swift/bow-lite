b02a505d5e09782b50e051477b63738fc45042e3
Tomás Ruiz López <truizlop@gmail.com>
2020-07-27 11:08:32 +0200

    Add docs for Id

--- Sources/BowLite/Data/Id/Id.swift
+++ Sources/BowLite/Data/Id/Id.swift
@@ -1,25 +1,34 @@
+/// The identity data type represents a context of having no effect on the type it wraps. A instance of `Id<Wrapped>` is isomorphic to an instance of `Wrapped`; it is just wrapped without any additional information.
 public struct Id<Wrapped> {
+    /// Wrapped value.
     public let value: Wrapped
     
+    /// Constructs a value of `Id`.
+    ///
+    /// - Parameter value: Value to be wrapped in `Id`.
     public init(_ value: Wrapped) {
         self.value = value
     }
 }
 
+// MARK: Instance of Semigroup for Id
 extension Id: Semigroup where Wrapped: Semigroup {
     public func combine(_ other: Id<Wrapped>) -> Id<Wrapped> {
         Id(self.value.combine(other.value))
     }
 }
 
+// MARK: Instance of Monoid for Id
 extension Id: Monoid where Wrapped: Monoid {
     public static var empty: Id<Wrapped> {
         Id(.empty)
     }
 }
 
+// MARK: Conformance of Equatable for Id
 extension Id: Equatable where Wrapped: Equatable {}
 
+// MARK: Conformance of CustomStringConvertible for Id
 extension Id: CustomStringConvertible where Wrapped: CustomStringConvertible {
     public var description: String {
         "Id(\(value.description))"
