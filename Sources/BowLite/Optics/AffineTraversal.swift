e8ea7772243224551f37f24d73707c112eb25dc7
Tomás Ruiz López <truizlop@gmail.com>
2020-07-28 17:37:09 +0200

    Add docs for AffineTraversal

--- Sources/BowLite/Optics/AffineTraversal.swift
+++ Sources/BowLite/Optics/AffineTraversal.swift
@@ -1,7 +1,20 @@
+/// An AffineTraversal is an optic that allows to see into a structure and getting, setting or modifying an optional focus.
+///
+/// An AffineTraversal can be seen as a weaker `Lens` and `Prism` and combines their weakest functions:
+///     - `set` meaning we can focus into a `Source` and set a value `Target`.
+///     - `get` meaning it returns the focus of an `AffineTraversal` (if present) or the original value.
 public struct AffineTraversal<Source, Target> {
+    /// Obtains the optional target, given a source
     public let get: (Source) -> Target?
+    
+    /// Creates a new source from the provided one, setting the target focused by this optic.
     public let set: (Source, Target) -> Source
     
+    /// Initializes an AffineTraversal
+    ///
+    /// - Parameters:
+    ///   - get: Getter function.
+    ///   - set: Setter function.
     public init(
         get: @escaping (Source) -> Target?,
         set: @escaping (Source, Target) -> Source) {
@@ -9,6 +22,12 @@ public struct AffineTraversal<Source, Target> {
         self.set = set
     }
     
+    /// Modifies the focus with a function.
+    ///
+    /// - Parameters:
+    ///   - source: Source.
+    ///   - transform: Modifying function.
+    /// - Returns: Modified source.
     public func modify(
         _ source: Source,
         _ transform: @escaping (Target) -> Target
@@ -16,12 +35,22 @@ public struct AffineTraversal<Source, Target> {
         modifyOptional(source, transform) ?? source
     }
     
+    /// Lifts a function modifying the focus to a function modifying the source.
+    ///
+    /// - Parameter transform: Modifying function.
+    /// - Returns: A function modifying the source.
     public func lift(
         _ transform: @escaping (Target) -> Target
     ) -> (Source) -> Source {
         { source in self.modify(source, transform) }
     }
     
+    /// Modifies the source with a function if it matches.
+    ///
+    /// - Parameters:
+    ///   - source: Source.
+    ///   - transform: Modifying function.
+    /// - Returns: An optional modified source that is present if the modification took place.
     public func modifyOptional(
         _ source: Source,
         _ transform: @escaping (Target) -> Target
@@ -31,6 +60,10 @@ public struct AffineTraversal<Source, Target> {
         }
     }
     
+    /// Joins with an AffineTraversal with the same focus.
+    ///
+    /// - Parameter affine: Value to join with.
+    /// - Returns: An AffineTraversal that operates in either of the original sources.
     public func choice<Other>(
         _ affine: AffineTraversal<Other, Target>
     ) -> AffineTraversal<Either<Source, Other>, Target> {
@@ -44,6 +77,9 @@ public struct AffineTraversal<Source, Target> {
         )
     }
     
+    /// Pairs this AffineTraversal with another type, placing this as the first element.
+    ///
+    /// - Returns: An AffineTraversal that operates on tuples where the second argument remains unchanged.
     public func first<Extra>() -> AffineTraversal<(Source, Extra), (Target, Extra)> {
         AffineTraversal<(Source, Extra), (Target, Extra)>(
             get: { source, extra in
@@ -55,6 +91,9 @@ public struct AffineTraversal<Source, Target> {
         )
     }
     
+    /// Pairs this AffineTraversal with another type, placing this as the second element.
+    ///
+    /// - Returns: An AffineTraversal that operates on tuples where the first argument remains unchanged.
     public func second<Extra>() -> AffineTraversal<(Extra, Source), (Extra, Target)> {
         AffineTraversal<(Extra, Source), (Extra, Target)>(
             get: { extra, source in
@@ -65,6 +104,10 @@ public struct AffineTraversal<Source, Target> {
         )
     }
     
+    /// Composes this value with an AffineTraversal.
+    ///
+    /// - Parameter affine: Value to compose with.
+    /// - Returns: An AffineTraversal resulting from the sequential application of the two provided optics.
     public func compose<NewTarget>(
         _ affine: AffineTraversal<Target, NewTarget>
     ) -> AffineTraversal<Source, NewTarget> {
@@ -81,6 +124,10 @@ public struct AffineTraversal<Source, Target> {
         )
     }
     
+    /// Composes this value with a Lens.
+    ///
+    /// - Parameter lens: Value to compose with.
+    /// - Returns: An AffineTraversal resulting from the sequential application of the two provided optics.
     public func compose<NewTarget>(
         _ lens: Lens<Target, NewTarget>
     ) -> AffineTraversal<Source, NewTarget> {
@@ -96,6 +143,10 @@ public struct AffineTraversal<Source, Target> {
         )
     }
     
+    /// Composes this value with a Traversal.
+    ///
+    /// - Parameter traversal: Value to compose with.
+    /// - Returns: A Traversal resulting from the sequential application of the two provided optics.
     public func compose<NewTarget>(
         _ traversal: Traversal<Target, NewTarget>
     ) -> Traversal<Source, NewTarget> {
@@ -110,6 +161,10 @@ public struct AffineTraversal<Source, Target> {
         )
     }
     
+    /// Composes this value with a Prism.
+    ///
+    /// - Parameter prism: Value to compose with.
+    /// - Returns: An AffineTraversal resulting from the sequential application of the two provided optics.
     public func compose<NewTarget>(
         _ prism: Prism<Target, NewTarget>
     ) -> AffineTraversal<Source, NewTarget> {
@@ -123,6 +178,11 @@ public struct AffineTraversal<Source, Target> {
         )
     }
     
+    /// Composes an AffineTraversal with an AffineTraversal.
+    /// - Parameters:
+    ///   - lhs: Left-hand side of the composition.
+    ///   - rhs: Right-hand side of the composition.
+    /// - Returns: An AffineTraversal resulting from the sequential application of the two provided optics.
     public static func +<NewTarget>(
         lhs: AffineTraversal<Source, Target>,
         rhs: AffineTraversal<Target, NewTarget>
@@ -130,6 +190,11 @@ public struct AffineTraversal<Source, Target> {
         lhs.compose(rhs)
     }
     
+    /// Composes an AffineTraversal with a Lens.
+    /// - Parameters:
+    ///   - lhs: Left-hand side of the composition.
+    ///   - rhs: Right-hand side of the composition.
+    /// - Returns: An AffineTraversal resulting from the sequential application of the two provided optics.
     public static func +<NewTarget>(
         lhs: AffineTraversal<Source, Target>,
         rhs: Lens<Target, NewTarget>
@@ -137,6 +202,11 @@ public struct AffineTraversal<Source, Target> {
         lhs.compose(rhs)
     }
     
+    /// Composes an AffineTraversal with a Prism.
+    /// - Parameters:
+    ///   - lhs: Left-hand side of the composition.
+    ///   - rhs: Right-hand side of the composition.
+    /// - Returns: An AffineTraversal resulting from the sequential application of the two provided optics.
     public static func +<NewTarget>(
         lhs: AffineTraversal<Source, Target>,
         rhs: Prism<Target, NewTarget>
@@ -144,6 +214,11 @@ public struct AffineTraversal<Source, Target> {
         lhs.compose(rhs)
     }
     
+    /// Composes an AffineTraversal with a Traversal.
+    /// - Parameters:
+    ///   - lhs: Left-hand side of the composition.
+    ///   - rhs: Right-hand side of the composition.
+    /// - Returns: A Traversal resulting from the sequential application of the two provided optics.
     public static func +<NewTarget>(
         lhs: AffineTraversal<Source, Target>,
         rhs: Traversal<Target, NewTarget>
@@ -151,12 +226,14 @@ public struct AffineTraversal<Source, Target> {
         lhs.compose(rhs)
     }
     
+    /// Converts this optic into a Traversal.
     public var asTraversal: Traversal<Source, Target> {
         Traversal<Source, Target>(modify: self.modify)
     }
 }
 
 public extension AffineTraversal where Source == Target {
+    /// Identity AffineTraversal
     static var identity: AffineTraversal<Source, Source> {
         AffineTraversal(
             get: id, set: { _, source in source }
@@ -165,6 +242,9 @@ public extension AffineTraversal where Source == Target {
 }
 
 public extension WritableKeyPath {
+    /// Obtains an AffineTraversal.
+    ///
+    /// - Returns: An AffineTraversal working on elements of type `Root` and focused on the `Target?` value provided by this key path.
     func affine<Target>() -> AffineTraversal<Root, Target> where Value == Target? {
         AffineTraversal(
             get: { source in source[keyPath: self] },
@@ -176,6 +256,10 @@ public extension WritableKeyPath {
         )
     }
     
+    /// Obtains an AffineTraversal focused on an index of an array.
+    ///
+    /// - Parameter index: Index for the focus of the optic.
+    /// - Returns: An AffineTraversal focused on an element of the array targeted by this key path.
     func index<Target>(_ index: Int) -> AffineTraversal<Root, Target> where Value == [Target] {
         AffineTraversal(
             get: { source in source[keyPath: self][safe: index] },
