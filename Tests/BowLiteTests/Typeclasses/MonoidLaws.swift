1d054984594ca7a5d9d44498524edbac3ba76716
Tomás Ruiz López <truizlop@gmail.com>
2020-07-29 12:48:26 +0200

    Add Monoid laws

---
+++ Tests/BowLiteTests/Typeclasses/MonoidLaws.swift
@@ -0,0 +1,21 @@
+import SwiftCheck
+import BowLite
+
+public class MonoidLaws<A: Monoid & Equatable & Arbitrary> {
+    public static func check() {
+        leftIdentity()
+        rightIdentity()
+    }
+    
+    private static func leftIdentity() {
+        property("Left identity") <~ forAll { (a: A) in
+            A.empty.combine(a) == a
+        }
+    }
+    
+    private static func rightIdentity() {
+        property("Right identity") <~ forAll { (a: A) in
+            a.combine(A.empty) == a
+        }
+    }
+}
