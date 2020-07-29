c0bccfec38c0c4b986e07bc7406169b7c1d888b1
Tomás Ruiz López <truizlop@gmail.com>
2020-07-29 13:37:00 +0200

    Test Const Applicative Laws

---
+++ Tests/BowLiteTests/Data/Const/Const+ApplicativeTest.swift
@@ -0,0 +1,152 @@
+import XCTest
+import SwiftCheck
+import BowLite
+
+class ConstApplicativeTest: XCTestCase {
+    func testApplicativeLaws() {
+        property("ap identity") <~ forAll() { (fa: Const<Int, Int>) in
+            
+            Const.pure(id).ap(fa)
+                ==
+            fa
+        }
+        
+        property("homomorphism") <~ forAll() { (a: Int, f: ArrowOf<Int, Int>) in
+            
+            Const.pure(f.getArrow).ap(Const<Int, Int>.pure(a))
+                ==
+            Const.pure(f.getArrow(a))
+        }
+        
+        property("interchange") <~ forAll() { (a: Int, b: Int) in
+            let fa = Const<Int, (Int) -> Int>.pure(constant(a) as (Int) -> Int)
+            
+            return fa.ap(Const.pure(b))
+                ==
+            Const.pure({ (x: (Int) -> Int) in x(a) } ).ap(fa)
+        }
+        
+        property("map derived") <~ forAll() { (fa: Const<Int, Int>, f: ArrowOf<Int, Int>) in
+            
+            fa.map(f.getArrow)
+                ==
+            Const.pure(f.getArrow).ap(fa)
+        }
+
+        property("Map with 2 inputs") <~ forAll { (a: Int, b: Int) in
+            
+            Const<Int, Int>.map(
+                .pure(a),
+                .pure(b)) { a1, b1 in
+                    a1 + b1
+            }
+                ==
+            Const<Int, Int>.pure(a + b)
+        }
+        
+        property("Map with 3 inputs") <~ forAll { (a: Int, b: Int, c: Int) in
+            
+            Const<Int, Int>.map(
+                .pure(a),
+                .pure(b),
+                .pure(c)) { a1, b1, c1 in
+                    a1 + b1 + c1
+            }
+                ==
+            Const<Int, Int>.pure(a + b + c)
+        }
+        
+        property("Map with 4 inputs") <~ forAll { (a: Int, b: Int, c: Int, d: Int) in
+            
+            Const<Int, Int>.map(
+                .pure(a),
+                .pure(b),
+                .pure(c),
+                .pure(d)) { a1, b1, c1, d1 in
+                    a1 + b1 + c1 + d1
+            }
+                ==
+            Const<Int, Int>.pure(a + b + c + d)
+        }
+        
+        property("Map with 5 inputs") <~ forAll { (a: Int, b: Int, c: Int, d: Int, e: Int) in
+            
+            Const<Int, Int>.map(
+                .pure(a),
+                .pure(b),
+                .pure(c),
+                .pure(d),
+                .pure(e)) { a1, b1, c1, d1, e1 in
+                    a1 + b1 + c1 + d1 + e1
+            }
+                ==
+            Const<Int, Int>.pure(a + b + c + d + e)
+        }
+
+        property("Map with 6 inputs") <~ forAll { (a: Int, b: Int, c: Int, d: Int, e: Int, f: Int) in
+            
+            Const<Int, Int>.map(
+                .pure(a),
+                .pure(b),
+                .pure(c),
+                .pure(d),
+                .pure(e),
+                .pure(f)) { a1, b1, c1, d1, e1, f1 in
+                    a1 + b1 + c1 + d1 + e1 + f1
+            }
+                ==
+            Const<Int, Int>.pure(a + b + c + d + e + f)
+        }
+        
+        property("Map with 7 inputs") <~ forAll { (a: Int, b: Int, c: Int, d: Int, e: Int, f: Int, g: Int) in
+            
+            Const<Int, Int>.map(
+                .pure(a),
+                .pure(b),
+                .pure(c),
+                .pure(d),
+                .pure(e),
+                .pure(f),
+                .pure(g)) { a1, b1, c1, d1, e1, f1, g1 in
+                    a1 + b1 + c1 + d1 + e1 + f1 + g1
+            }
+                ==
+            Const<Int, Int>.pure(a + b + c + d + e + f + g)
+        }
+        
+        property("Map with 8 inputs") <~ forAll { (a: Int, b: Int, c: Int, d: Int, e: Int, f: Int, g: Int, h: Int) in
+            
+            Const<Int, Int>.map(
+                .pure(a),
+                .pure(b),
+                .pure(c),
+                .pure(d),
+                .pure(e),
+                .pure(f),
+                .pure(g),
+                .pure(h)) { a1, b1, c1, d1, e1, f1, g1, h1 in
+                    a1 + b1 + c1 + d1 + e1 + f1 + g1 + h1
+            }
+                ==
+            Const<Int, Int>.pure(a + b + c + d + e + f + g + h)
+        }
+        
+        property("Map with 9 inputs") <~ forAll { (a: Int, b: Int, c: Int, d: Int, e: Int, f: Int, g: Int, h: Int) in
+            
+            Const<Int, Int>.map(
+                .pure(a),
+                .pure(b),
+                .pure(c),
+                .pure(d),
+                .pure(e),
+                .pure(f),
+                .pure(g),
+                .pure(h),
+                .pure(a)) { a1, b1, c1, d1, e1, f1, g1, h1, i1 in
+                    a1 + b1 + c1 + d1 + e1 + f1 + g1 + h1 + i1
+            }
+                ==
+            Const<Int, Int>.pure(a + b + c + d + e + f + g + h + a)
+        }
+    }
+}
