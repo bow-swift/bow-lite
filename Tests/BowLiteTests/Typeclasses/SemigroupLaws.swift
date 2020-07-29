5b09692e81e5a1cdc76beacccbf67e6a99e436da
Tomás Ruiz López <truizlop@gmail.com>
2020-07-29 12:48:18 +0200

    Add Semigroup laws

---
+++ Tests/BowLiteTests/Typeclasses/SemigroupLaws.swift
@@ -0,0 +1,25 @@
+import SwiftCheck
+import BowLite
+
+public class SemigroupLaws<A: Semigroup & Arbitrary & Equatable> {
+    public static func check() {
+        associativity()
+        reduction()
+    }
+    
+    private static func associativity() {
+        property("Associativity") <~ forAll { (a: A, b: A, c: A) in
+            a.combine(b).combine(c)
+                ==
+            a.combine(b.combine(c))
+        }
+    }
+    
+    private static func reduction() {
+        property("Reduction") <~ forAll { (a: A, b: A, c: A) in
+            A.combineAll(a, b, c)
+                ==
+            a.combine(b).combine(c)
+        }
+    }
+}
