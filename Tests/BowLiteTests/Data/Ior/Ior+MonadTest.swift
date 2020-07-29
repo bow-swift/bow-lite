3f7859dcab91c0a336228e6fd3db159cb6f352a5
Tomás Ruiz López <truizlop@gmail.com>
2020-07-29 16:18:28 +0200

    Test Ior Monad Laws

---
+++ Tests/BowLiteTests/Data/Ior/Ior+MonadTest.swift
@@ -0,0 +1,53 @@
+import XCTest
+import SwiftCheck
+import BowLite
+
+class IorMonadTest: XCTestCase {
+    func testMonadLaws() {
+        property("Monad left identity") <~ forAll { (a: Int, f: ArrowOf<Int, Int>) in
+            let g = f.getArrow >>> Ior<Int, Int>.pure
+            
+            return Ior.pure(a).flatMap(g)
+                ==
+            g(a)
+        }
+        
+        property("Monad right identity") <~ forAll { (a: Int) in
+            let fa = Ior<Int, Int>.pure(a)
+            return fa.flatMap(Ior.pure)
+                ==
+            fa
+        }
+        
+        property("Kleisli left identity") <~ forAll { (a: Int, f: ArrowOf<Int, Int>) in
+            let g = f.getArrow >>> Ior<Int, Int>.pure
+            
+            return ({ (n: Int) in Ior<Int, Int>.pure(n) } >=> g)(a)
+                ==
+            g(a)
+        }
+        
+        property("Kleisli right identity") <~ forAll { (a: Int, f: ArrowOf<Int, Int>) in
+            let g = f.getArrow >>> Ior<Int, Int>.pure
+            
+            return (g >=> Ior.pure)(a)
+                ==
+            g(a)
+        }
+        
+        property("Monad flatMap coherence") <~ forAll { (a: Int, f: ArrowOf<Int, Int>) in
+            let g = f.getArrow >>> Ior<Int, Int>.pure
+            let fa = Ior<Int, Int>.pure(a)
+            
+            return fa.flatMap(g)
+                ==
+            fa.map(f.getArrow)
+        }
+        
+        property("Flatten") <~ forAll { (a: Int) in
+            Ior<Int, Ior<Int, Int>>.pure(Ior.pure(a)).flatten()
+                ==
+            Ior<Int, Int>.pure(a)
+        }
+    }
+}
