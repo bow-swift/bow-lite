b60008a1cdf6e7039221c2ed9e3b9c69c03d087f
Tomás Ruiz López <truizlop@gmail.com>
2020-07-29 12:50:29 +0200

    Add CustomStringConvertibleLaws

---
+++ Tests/BowLiteTests/Typeclasses/CustomStringConvertibleLaws.swift
@@ -0,0 +1,16 @@
+import SwiftCheck
+
+public class CustomStringConvertibleLaws<A: CustomStringConvertible & Arbitrary> {
+    public static func check(){
+        equality()
+    }
+    
+    private static func equality() {
+        property("Equal objects must show equal content") <~ forAll { (a: A) in
+            let x1 = a
+            let x2 = a
+            return x1.description == x2.description
+        }
+    }
+    
+}
