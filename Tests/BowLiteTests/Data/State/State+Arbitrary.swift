8b31765eeb7ce4ac1d5174c7ab0df2acd8753f37
Tomás Ruiz López <truizlop@gmail.com>
2020-07-29 11:48:08 +0200

    Add generator for State

---
+++ Tests/BowLiteTests/Data/State/State+Arbitrary.swift
@@ -0,0 +1,14 @@
+import SwiftCheck
+import BowLite
+
+extension State: Arbitrary where StateType: Hashable & Arbitrary & CoArbitrary, Value: Arbitrary {
+    
+    public static var arbitrary: Gen<State<StateType, Value>> {
+        ArrowOf<StateType, Pair<StateType, Value>>.arbitrary.map { arrow in
+            State { state in
+                let pair = arrow.getArrow(state)
+                return (pair.first, pair.second)
+            }
+        }
+    }
+}
