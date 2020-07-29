26b710f6f752868ceb62cdcd557b757605222eb2
Tomás Ruiz López <truizlop@gmail.com>
2020-07-29 16:21:45 +0200

    Add Kleisli composition for Pair

--- Sources/BowLite/Data/Kleilsi/Kleisli.swift
+++ Sources/BowLite/Data/Kleilsi/Kleisli.swift
@@ -423,3 +423,31 @@ public func >=><A, B, C, D, E>(
 ) -> (A) -> EnvIO<D, E, C> {
     andThen(f, g)
 }
+
+/// Composes two effectful functions.
+///
+/// - Parameters:
+///   - f: Left-hand side of the composition.
+///   - g: Right-hand side of the composition.
+/// - Returns: A function resulting from the Kleisli composition of the two effectful functions provided.
+public func andThen<A, B, C, D: Monoid>(
+    _ f: @escaping (A) -> Pair<D, B>,
+    _ g: @escaping (B) -> Pair<D, C>
+) -> (A) -> Pair<D, C> {
+    { a in
+        f(a).flatMap(g)
+    }
+}
+
+/// Composes two effectful functions.
+///
+/// - Parameters:
+///   - f: Left-hand side of the composition.
+///   - g: Right-hand side of the composition.
+/// - Returns: A function resulting from the Kleisli composition of the two effectful functions provided.
+public func >=><A, B, C, D: Monoid>(
+    _ f: @escaping (A) -> Pair<D, B>,
+    _ g: @escaping (B) -> Pair<D, C>
+) -> (A) -> Pair<D, C> {
+    andThen(f, g)
+}
