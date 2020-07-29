e28dc56b7a6515ed8d33df0f20ffea5dfd0dee59
Tomás Ruiz López <truizlop@gmail.com>
2020-07-29 16:23:46 +0200

    Test Try Monad Laws

---
+++ Tests/BowLiteTests/Data/Try/File.swift
@@ -0,0 +1,53 @@
+import XCTest
+import SwiftCheck
+import BowLite
+
+class TryMonadTest: XCTestCase {
+    func testMonadLaws() {
+        property("Monad left identity") <~ forAll { (a: Int, f: ArrowOf<Int, Int>) in
+            let g = f.getArrow >>> Try<Int>.pure
+            
+            return Try.pure(a).flatMap(g)
+                ==
+            g(a)
+        }
+        
+        property("Monad right identity") <~ forAll { (a: Int) in
+            let fa = Try<Int>.pure(a)
+            return fa.flatMap(Try.pure)
+                ==
+            fa
+        }
+        
+        property("Kleisli left identity") <~ forAll { (a: Int, f: ArrowOf<Int, Int>) in
+            let g = f.getArrow >>> Try<Int>.pure
+            
+            return ({ (n: Int) in Try<Int>.pure(n) } >=> g)(a)
+                ==
+            g(a)
+        }
+        
+        property("Kleisli right identity") <~ forAll { (a: Int, f: ArrowOf<Int, Int>) in
+            let g = f.getArrow >>> Try<Int>.pure
+            
+            return (g >=> Try.pure)(a)
+                ==
+            g(a)
+        }
+        
+        property("Monad flatMap coherence") <~ forAll { (a: Int, f: ArrowOf<Int, Int>) in
+            let g = f.getArrow >>> Try<Int>.pure
+            let fa = Try<Int>.pure(a)
+            
+            return fa.flatMap(g)
+                ==
+            fa.map(f.getArrow)
+        }
+        
+        property("Flatten") <~ forAll { (a: Int) in
+            Try<Try<Int>>.pure(Try.pure(a)).flatten()
+                ==
+            Try<Int>.pure(a)
+        }
+    }
+}
