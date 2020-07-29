12f898e630e08187dce548c033bc3f8ee658a7c9
Tomás Ruiz López <truizlop@gmail.com>
2020-07-29 13:13:55 +0200

    Test State Functor Laws

---
+++ Tests/BowLiteTests/Data/State/State+FunctorTest.swift
@@ -0,0 +1,42 @@
+import XCTest
+import SwiftCheck
+import BowLite
+
+class StateFunctorTest: XCTestCase {
+    func testFunctorLaws() {
+        property("Identity is preserved under functor transformation") <~ forAll() { (fa: State<Int, Int>) in
+            fa.map(id)(0)
+                ==
+            id(fa)(0)
+        }
+        
+        property("Composition is preserved under functor transformation") <~ forAll() { (fa: State<Int, Int>, f: ArrowOf<Int, Int>, g: ArrowOf<Int, Int>) in
+            
+            fa.map(f.getArrow).map(g.getArrow)(0)
+                ==
+            fa.map(f.getArrow >>> g.getArrow)(0)
+        }
+
+        property("product") <~ forAll { (fa: State<Int, Int>, f: ArrowOf<Int, Int>) in
+            
+            fa.product(f.getArrow).map { x in x.1 }(0)
+                ==
+            fa.map(f.getArrow)(0)
+        }
+        
+        property("tuple left") <~ forAll { (fa: State<Int, Int>, b: Int) in
+            
+            fa.tupleLeft(b).map { x in x.0 }(0)
+                ==
+            fa.as(b)(0)
+        }
+        
+        property("tuple right") <~ forAll { (fa: State<Int, Int>, b: Int) in
+            
+            fa.tupleRight(b).map { x in x.1 }(0)
+                ==
+            fa.as(b)(0)
+        }
+    }
+}
+
