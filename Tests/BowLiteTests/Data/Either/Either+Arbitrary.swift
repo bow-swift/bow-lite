c582e42a74d3ae196d7389e4216bee1d1fe01c96
Tomás Ruiz López <truizlop@gmail.com>
2020-07-29 11:03:12 +0200

    Add generator for Either

---
+++ Tests/BowLiteTests/Data/Either/Either+Arbitrary.swift
@@ -0,0 +1,10 @@
+import SwiftCheck
+import BowLite
+
+extension Either: Arbitrary where Left: Arbitrary, Right: Arbitrary {
+    public static var arbitrary: Gen<Either<Left, Right>> {
+        let left = Left.arbitrary.map(Either.left)
+        let right = Right.arbitrary.map(Either.right)
+        return Gen.one(of: [left, right])
+    }
+}
