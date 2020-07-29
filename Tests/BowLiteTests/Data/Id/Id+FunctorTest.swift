9fe41fbbd7eed0cedcd686cdd73ff60524d2b579
Tomás Ruiz López <truizlop@gmail.com>
2020-07-29 13:10:25 +0200

    Test Id Functor Laws

---
+++ Tests/BowLiteTests/Data/Id/Id+FunctorTest.swift
@@ -0,0 +1,42 @@
+import XCTest
+import SwiftCheck
+import BowLite
+
+class IdFunctorTest: XCTestCase {
+    func testFunctorLaws() {
+        property("Identity is preserved under functor transformation") <~ forAll() { (fa: Id<Int>) in
+            fa.map(id)
+                ==
+            id(fa)
+        }
+        
+        property("Composition is preserved under functor transformation") <~ forAll() { (fa: Id<Int>, f: ArrowOf<Int, Int>, g: ArrowOf<Int, Int>) in
+            
+            fa.map(f.getArrow).map(g.getArrow)
+                ==
+            fa.map(f.getArrow >>> g.getArrow)
+        }
+
+        property("product") <~ forAll { (fa: Id<Int>, f: ArrowOf<Int, Int>) in
+            
+            fa.product(f.getArrow).map { x in x.1 }
+                ==
+            fa.map(f.getArrow)
+        }
+        
+        property("tuple left") <~ forAll { (fa: Id<Int>, b: Int) in
+            
+            fa.tupleLeft(b).map { x in x.0 }
+                ==
+            fa.as(b)
+        }
+        
+        property("tuple right") <~ forAll { (fa: Id<Int>, b: Int) in
+            
+            fa.tupleRight(b).map { x in x.1 }
+                ==
+            fa.as(b)
+        }
+    }
+}
+
