e0770a507019fb04b8e50a2194679086ca2d32d9
Tomás Ruiz López <truizlop@gmail.com>
2020-07-30 17:38:28 +0200

    Test IO ApplicativeError Laws

---
+++ Tests/BowLiteTests/Effects/IO/IO+ApplicativeErrorTest.swift
@@ -0,0 +1,28 @@
+import XCTest
+import SwiftCheck
+import BowLite
+
+class IOApplicativeErrorTest: XCTestCase {
+    func testApplicativeErrorLaws() {
+        property("Applicative error handle") <~ forAll { (error: AnyError, f: ArrowOf<AnyError, Int>) in
+            
+            IO.raiseError(error).handleError(f.getArrow)
+                ==
+            IO.pure(f.getArrow(error))
+        }
+        
+        property("Applicative error handle with") <~ forAllNoShrink(ioGen, AnyError.arbitrary) { (f: IO<AnyError, Int>, error: AnyError) in
+            
+            IO.raiseError(error).handleErrorWith(constant(f))
+                ==
+            constant(f)(error)
+        }
+        
+        property("Applicative error handle with pure") <~ forAllNoShrink(Int.arbitrary, ioGen) { (a: Int, f: IO<AnyError, Int>) in
+            
+            IO.pure(a).handleErrorWith(constant(f))
+                ==
+            IO.pure(a)
+        }
+    }
+}
