f5e84734087c4fa03e0df27f7ad793a6fcc910cf
Tomás Ruiz López <truizlop@gmail.com>
2020-07-30 15:47:03 +0200

    Test Validated ApplicativeError Laws

---
+++ Tests/BowLiteTests/Data/Validated/Validated+ApplicativeErrorTest.swift
@@ -0,0 +1,28 @@
+import XCTest
+import SwiftCheck
+import BowLite
+
+class ValidatedApplicativeErrorTest: XCTestCase {
+    func testApplicativeErrorLaws() {
+        property("Applicative error handle") <~ forAll { (error: Int, f: ArrowOf<Int, Int>) in
+            
+            Validated.raiseError(error).handleError(f.getArrow)
+                ==
+            Validated.pure(f.getArrow(error))
+        }
+        
+        property("Applicative error handle with") <~ forAll { (f: ArrowOf<Int, Validated<Int, Int>>, error: Int) in
+            
+            Validated.raiseError(error).handleErrorWith(f.getArrow)
+                ==
+            f.getArrow(error)
+        }
+        
+        property("Applicative error handle with pure") <~ forAll { (a: Int, f: ArrowOf<Int, Validated<Int, Int>>) in
+            
+            Validated.pure(a).handleErrorWith(f.getArrow)
+                ==
+            Validated.pure(a)
+        }
+    }
+}
