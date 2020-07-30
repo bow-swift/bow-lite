e8ee6d553bdd55143aedbfaf9fdd7651596b64e6
Tomás Ruiz López <truizlop@gmail.com>
2020-07-30 15:39:56 +0200

    Test Result ApplicativeError Laws

---
+++ Tests/BowLiteTests/Data/Result/Result+ApplicativeErrorTest.swift
@@ -0,0 +1,28 @@
+import XCTest
+import SwiftCheck
+import BowLite
+
+class ResultApplicativeErrorTest: XCTestCase {
+    func testApplicativeErrorLaws() {
+        property("Applicative error handle") <~ forAll { (error: AnyError, f: ArrowOf<AnyError, Int>) in
+            
+            Result.raiseError(error).handleError(f.getArrow)
+                ==
+            Result.pure(f.getArrow(error))
+        }
+        
+        property("Applicative error handle with") <~ forAll { (f: ArrowOf<AnyError, Result<Int, AnyError>>, error: AnyError) in
+            
+            Result.raiseError(error).handleErrorWith(f.getArrow)
+                ==
+            f.getArrow(error)
+        }
+        
+        property("Applicative error handle with pure") <~ forAll { (a: Int, f: ArrowOf<AnyError, Result<Int, AnyError>>) in
+            
+            Result.pure(a).handleErrorWith(f.getArrow)
+                ==
+            Result.pure(a)
+        }
+    }
+}
