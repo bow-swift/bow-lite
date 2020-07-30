008c6c99cebcfec48080899fe079afdfad7b4a58
Tomás Ruiz López <truizlop@gmail.com>
2020-07-30 17:40:13 +0200

    Test EnvIO ApplicativeError Laws

---
+++ Tests/BowLiteTests/Effects/EnvIO/EnvIO+ApplicativeErrorTest.swift
@@ -0,0 +1,28 @@
+import XCTest
+import SwiftCheck
+import BowLite
+
+class EnvIOApplicativeErrorTest: XCTestCase {
+    func testApplicativeErrorLaws() {
+        property("Applicative error handle") <~ forAll { (error: AnyError, f: ArrowOf<AnyError, Int>) in
+            
+            EnvIO.raiseError(error).handleError(f.getArrow)
+                ==
+            EnvIO.pure(f.getArrow(error))
+        }
+        
+        property("Applicative error handle with") <~ forAllNoShrink(envIOGen, AnyError.arbitrary) { (f: EnvIO<Int, AnyError, Int>, error: AnyError) in
+            
+            EnvIO.raiseError(error).handleErrorWith(constant(f))
+                ==
+            constant(f)(error)
+        }
+        
+        property("Applicative error handle with pure") <~ forAllNoShrink(Int.arbitrary, envIOGen) { (a: Int, f: EnvIO<Int, AnyError, Int>) in
+            
+            EnvIO.pure(a).handleErrorWith(constant(f))
+                ==
+            EnvIO.pure(a)
+        }
+    }
+}
