eeea1ab936f62d3430d75df7c61d87cb04e8fe18
Tomás Ruiz López <truizlop@gmail.com>
2020-08-04 10:22:36 +0200

    Make EnvIO compatible with Swift 5.1

--- Tests/BowLiteTests/Effects/EnvIO/EnvIO+Equatable.swift
+++ Tests/BowLiteTests/Effects/EnvIO/EnvIO+Equatable.swift
@@ -2,6 +2,6 @@ import BowLite
 
 extension EnvIO: Equatable where Dependencies == Int, Failure: Equatable, Success: Equatable {
     public static func ==(lhs: EnvIO<Dependencies, Failure, Success>, rhs: EnvIO<Dependencies, Failure, Success>) -> Bool {
-        lhs(0) == rhs(0)
+        lhs.provide(0) == rhs.provide(0)
     }
 }
