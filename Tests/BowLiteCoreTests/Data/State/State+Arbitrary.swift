908dbc518f8d0e640f0adcccb22fc3ae100f447d
Miguel Ángel Díaz <madiaz.dev@gmail.com>
2020-08-10 15:16:46 +0200

    fix tests in linux

--- Tests/BowLiteCoreTests/Data/State/State+Arbitrary.swift
+++ Tests/BowLiteCoreTests/Data/State/State+Arbitrary.swift
@@ -2,14 +2,24 @@ import SwiftCheck
 import BowLiteLaws
 import BowLiteCore
 
-extension State: Arbitrary where StateType: Hashable & Arbitrary & CoArbitrary, Value: Arbitrary {
-    
+extension State: Arbitrary where StateType: Hashable & CoArbitrary & Arbitrary, Value: Arbitrary {
     public static var arbitrary: Gen<State<StateType, Value>> {
-        ArrowOf<StateType, Pair<StateType, Value>>.arbitrary.map { arrow in
+        ArrowOf<StateType, Tuple<StateType, Value>>.arbitrary.map { arrow in
             State { state in
                 let pair = arrow.getArrow(state)
                 return (pair.first, pair.second)
             }
         }
+    }
+}
+
+private struct Tuple<A, B> {
+    let first: A
+    let second: B
+}
+
+extension Tuple: Arbitrary where A: Arbitrary, B: Arbitrary {
+    static var arbitrary: Gen<Tuple<A, B>> {
+        Gen.zip(A.arbitrary, B.arbitrary).map(Tuple.init)
     }
 }
